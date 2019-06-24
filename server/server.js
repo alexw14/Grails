9d61983f8c15939612dbbdb1186e0239360c5f89
Alex Wong <alexw14@gmail.com>
2019-06-23 17:20:30 -0700

    update server.js and add user model

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Models
+const { User } = require('./models/user');
+
+// Users
+app.post('/api/users/register', (req, res) => {
+  res.status(200);
+})
+
 
 const port = process.env.PORT || 3002;
 
