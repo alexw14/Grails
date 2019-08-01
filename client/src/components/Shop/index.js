f021d790fb775f93dc38598266daec22431a5239
Alex Wong <alexw14@gmail.com>
2019-07-31 22:40:38 -0700

    Add Collapsible Checkbox component to Shop page

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -4,14 +4,36 @@ import PageTopSection from '../utils/pageTopSection';
 import { connect } from 'react-redux';
 import { getBrands, getCategories } from '../../actions/products_actions';
 
+import CollapsibleCheckbox from '../utils/collapsibleCheckbox';
+
 class Shop extends Component {
+
+  state = {
+    grid: '',
+    limit: 6,
+    skip: 0,
+    filters: {
+      brand: [],
+      category: [],
+      price: []
+    }
+  }
 
   componentDidMount() {
     this.props.dispatch(getBrands());
     this.props.dispatch(getCategories());
   }
 
+  handleFilters = (filters, category) => {
+    const newFilters = { ...this.state.filters };
+    newFilters[category] = filters;
+    this.setState({
+      filters: newFilters
+    });
+  }
+
   render() {
+    console.log(this.state.filters)
     const products = this.props.products;
     return (
       <div>
@@ -21,7 +43,21 @@ class Shop extends Component {
         <div className="container">
           <div className="shop_wrapper">
             <div className="left">
-              Left
+
+              <CollapsibleCheckbox
+                initState={true}
+                title='Brands'
+                list={products.byBrands}
+                handleFilters={(filters) => this.handleFilters(filters, 'brand')}
+              />
+              <CollapsibleCheckbox
+                initState={false}
+                title='Categories'
+                list={products.byCategories}
+                handleFilters={(filters) => this.handleFilters(filters, 'category')}
+              />
+
+
             </div>
             <div className="right">
               Right
