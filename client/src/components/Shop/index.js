b59cc5f6ba80a8ed7104f9c4251ae4fb58c4ea9a
Alex Wong <alexw14@gmail.com>
2019-08-01 21:41:11 -0700

    Add collapsible radio button component for filtering price

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -3,8 +3,9 @@ import PageTopSection from '../utils/pageTopSection';
 
 import { connect } from 'react-redux';
 import { getBrands, getCategories } from '../../actions/products_actions';
-
+import { price } from '../utils/Form/fixed_categories';
 import CollapsibleCheckbox from '../utils/collapsibleCheckbox';
+import CollapsibleRadio from '../utils/collapsibleRadio';
 
 class Shop extends Component {
 
@@ -24,16 +25,30 @@ class Shop extends Component {
     this.props.dispatch(getCategories());
   }
 
+  handlePrice = (value) => {
+    const data = price;
+    let array = [];
+    for (let key in data) {
+      if (data[key]._id === parseInt(value, 10)) {
+        array = data[key].array;
+      }
+    }
+    return array;
+  }
+
   handleFilters = (filters, category) => {
     const newFilters = { ...this.state.filters };
     newFilters[category] = filters;
+    if (category === 'price') {
+      let priceValues = this.handlePrice(filters);
+      newFilters[category] = priceValues;
+    }
     this.setState({
       filters: newFilters
     });
   }
 
   render() {
-    console.log(this.state.filters)
     const products = this.props.products;
     return (
       <div>
@@ -56,7 +71,12 @@ class Shop extends Component {
                 list={products.byCategories}
                 handleFilters={(filters) => this.handleFilters(filters, 'category')}
               />
-
+              <CollapsibleRadio
+                initState={true}
+                title="Price"
+                list={price}
+                handleFilters={(filters) => this.handleFilters(filters, 'price')}
+              />
 
             </div>
             <div className="right">
